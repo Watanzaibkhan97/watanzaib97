a47141fb24c00fd447768a8dda3171681f8eb8d8
Watanzaibkhan97 <watanzaibkhanp2@gmail.com>
2026-03-12 21:42:14 +0500

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,27 @@
+particlesJS("canvas-container", {
+
+particles: {
+
+number: {
+value: 80
+},
+
+size: {
+value: 3
+},
+
+move: {
+speed: 2
+},
+
+line_linked: {
+enable: true
+},
+
+color: {
+value: "#00d4ff"
+}
+
+}
+
+})
